0e99f25a3532f14aab67ea1dc4daed1d5594b215
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-03-18 23:15:32 +0100

    add initial homeview and install svg loader

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,18 @@
   </div>
 </template>
 
-<script>
-const express = require("express");
-const cors = require("cors");
-const app = express();
+ <script>
+// const express = require("express"); Bör flyttas ut!
+// const cors = require("cors");
+// const app = express();
 
-const beansRouter = require("./routes/beans");
-const PORT = 5000;
+// const beansRouter = require("./routes/beans");
+// const PORT = 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/beans", beansRouter);
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/beans", beansRouter);
 
-app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+// app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+//
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,22 @@
 <template>
-  <div id="Home"></div>
+  <div id="Home">
+    <homeLogo />
+
+    <div id="homeTitle">
+      <h1>AIR BEAN</h1>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
 </template>
+<script>
+import homeLogo from "../assets/graphics/airbean-landing.svg";
+
+export default {
+  name: "Home",
+  components: {
+    homeLogo
+  }
+};
+</script>
+<style scoped></style>
