1dbe7dcee133337d110d2dd2d70b5404ac7c5720
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-03-19 16:55:19 +0100

    add styling to the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,56 @@
 <template>
   <div id="Home">
-    <homeLogo />
-
-    <div id="homeTitle">
-      <h1>AIR BEAN</h1>
+    <div id="homeImgContainer">
+      <homeImgRight id="homeImgRight" />
+      <div id="homeLogoContainer">
+        <homeLogo id="homeLogo" />
+      </div>
+      <homeImgLeft id="homeImgLeft" />
     </div>
-    <div></div>
-    <div></div>
   </div>
 </template>
 <script>
 import homeLogo from "../assets/graphics/airbean-landing.svg";
+import homeImgRight from "../assets/graphics/intro-graphic-left.svg";
+import homeImgLeft from "../assets/graphics/intro-graphic-right.svg";
 
 export default {
   name: "Home",
   components: {
-    homeLogo
+    homeLogo,
+    homeImgRight,
+    homeImgLeft
   }
 };
 </script>
-<style scoped></style>
+<style scoped>
+#Home {
+  background-color: rgb(61, 126, 98);
+}
+
+#homeLogoContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#homeLogo {
+  position: absolute;
+}
+
+#homeImgContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+#homeImgRight {
+  display: flex;
+  justify-content: flex-end;
+}
+
+#homeImgLeft {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
