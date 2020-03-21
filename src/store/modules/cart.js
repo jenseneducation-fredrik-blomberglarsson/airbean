4461e77e14507336a78d1d4e05571612ea8b2fc6
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-03-21 18:18:24 +0100

    Update cart.js

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,8 +1,30 @@
-const state = {};
+const state = {
+  cart: []
+};
 
-const mutations = {};
-const actions = {};
-const getters = {};
+const mutations = {
+  setCart(state, cart) {
+    state.cart = cart;
+  }
+};
+const actions = {
+  addMenuItem({ state, commit }, item) {
+    let cart = state.cart;
+    cart.push(item);
+    commit("setCart", cart);
+  },
+  removeMenuItem({ state, commit }, item) {
+    let cart = state.cart;
+    let firstOccuranceIndex = cart.findIndex(i => i.id === item.id);
+    if (firstOccuranceIndex !== -1) {
+      cart.splice(firstOccuranceIndex, 1);
+    }
+    commit("setCart", cart);
+  }
+};
+const getters = {
+  cart: state => state.cart
+};
 
 export default {
   state,
