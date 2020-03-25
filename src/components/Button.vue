15aa795171e649bc4a3af4f9c8cba9c15e67a445
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-03-25 10:23:32 +0100

    Create Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="button" v-on:click="onClick()" v-bind:style="{'background-color':backgroundColor}">
+    <p v-bind:style="{color:textColor}">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    backgroundColor: String,
+    textColor: String,
+    title: String,
+    onClick: Function
+  }
+};
+</script>
+
+<style scoped>
+#button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4em;
+  border-radius: 2em;
+  padding-left: 2em;
+  padding-right: 2em;
+  cursor: pointer;
+}
+
+p {
+  font-size: 2em;
+  margin: 0;
+}
+</style>
