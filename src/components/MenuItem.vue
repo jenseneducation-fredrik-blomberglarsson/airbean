363826d5d09c28af77399176b104f39acdcc370b
fredrikblomberglarsson <fredrik.blomberg.larsson@gmail.com>
2020-03-31 15:26:34 +0200

    fixed white line & price

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -3,12 +3,10 @@
     <aside @click="addMenuItem(item)">
       <img class="add" src="./../assets/graphics/add.svg" alt />
     </aside>
-    <div class="items">
-      <h3 class="title">{{item.title}}</h3>
-      <hr class="dots" />
-      <p class="desc">{{item.desc}}</p>
-      <h3 class="price">{{item.price}}</h3>
-    </div>
+    <h3 class="title">{{item.title}}</h3>
+    <hr class="dots" />
+    <p class="desc">{{item.desc}}</p>
+    <h3 class="price">{{item.price}}</h3>
   </div>
 </template>
 
@@ -25,10 +23,11 @@ export default {
   display: grid;
   grid-template-columns: 5rem 1fr 3rem;
   grid-template-rows: 2rem 2rem;
-  margin: 0.5rem;
+  margin: 0;
+  height: 100vh;
 }
 .add {
-  margin-top: 3%;
+  margin-top: 8%;
   margin-left: 45%;
   width: 23%;
   cursor: pointer;
@@ -38,25 +37,35 @@ export default {
   background-color: black;
 }
 .title {
+  grid-column: 2/3;
+  grid-row: 1/2;
   font-family: "PT Serif", serif;
   font-size: 1.5rem;
   margin: 0;
   padding: 0;
 }
+
 .price {
-  margin: 0 1.5rem;
+  margin: 0;
   font-size: 1.5rem;
   text-align: right;
+  grid-column: 3/4;
+  grid-row: 1/3;
+  text-align: left;
 }
 .desc {
+  grid-column: 2/3;
+  grid-row: 2/3;
   font-family: "Work Sans", sans-serif;
   margin: 0;
   padding: 0;
 }
 .dots {
-  margin: 0 3.5rem;
+  margin: 0;
+  grid-column: 2/3;
+  grid-row: 2/2;
   border: none;
   border-top: 1px dotted rgb(0, 0, 0);
-  width: 70%;
+  width: 100%;
 }
 </style>
